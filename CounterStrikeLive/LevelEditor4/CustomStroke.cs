ffacf04d3409c60a5616963df601b677dd76e5fc
mastert4b <mastert4b@dd554dd6-d52b-11dd-a3c7-fda619b15ae3>
2008-12-29 23:16:37 +0000

    buggy version

---
+++ CounterStrikeLive/LevelEditor4/CustomStroke.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Ink;
+using System.Windows.Input;
+
+namespace CSL.LevelEditor
+{
+    public class CustomStroke : Stroke
+    {
+        public CustomStroke(StylusPointCollection stylusPointCollection)
+            : base(stylusPointCollection)
+        {
+        }
+        //public string _ImagePath;
+    }
+}
