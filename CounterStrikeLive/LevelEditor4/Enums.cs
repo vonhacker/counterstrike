f1326e989b08be3f8c5327f25b480767cdf539e8
mastert4b <mastert4b@dd554dd6-d52b-11dd-a3c7-fda619b15ae3>
2008-12-30 23:50:18 +0000

    canvas is crap

--- CounterStrikeLive/LevelEditor4/Enums.cs
+++ CounterStrikeLive/LevelEditor4/Enums.cs
@@ -5,8 +5,21 @@ using System.Text;
 
 namespace CSL.LevelEditor
 {
-    public enum CustomMode
+    /// <summary>
+    /// Our Editor has different modes for different operations
+    /// </summary>
+    public enum EditorMode
     {
-        select, ink, polygon, erase
+        Select = 1,
+        Ink =2,
+        /// <summary>
+        /// We can draw Polygons in this mode
+        /// </summary>
+        Polygon = 3, 
+        /// <summary>
+        /// We selected some point and now its in the moving state(to be placed somewhere else)
+        /// </summary>
+        MovingPoint = 4,
+        Erase = 5,
     }
 }
--- CounterStrikeLive/LevelEditor4/MapDatabase.cs
+++ CounterStrikeLive/LevelEditor4/MapDatabase.cs
@@ -17,18 +17,18 @@ namespace CSL.LevelEditor
             public double Y;
             public string Path;
         }
-        public Point _TStartPos;
-        public Point _CStartPos;
-        public List<Layer> _Layers = new List<Layer>();
+        public Point TerroristStartPos;
+        public Point CounterTerroristStartPos;
+        public List<Layer> Layers = new List<Layer>();
         public class Layer
         {
-            public List<Image> _Images = new List<Image>();
-            public List<Polygon> _Polygons = new List<Polygon>();
+            public List<Image> Images = new List<Image>();
+            public List<Polygon> Polygons = new List<Polygon>();
         }
         public class Polygon
         {
-            public Color _Color;
-            public List<Point> _Points = new List<Point>();
+            public Color Color;
+            public List<Point> Points = new List<Point>();
         }
     }
 }
