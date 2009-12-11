a5a20bdd7ebce3d65c7ae56d4ec288c539753cc2
dorumonstr <dorumonstr@dd554dd6-d52b-11dd-a3c7-fda619b15ae3>
2009-12-11 18:35:08 +0000

    work 3 players , onmousedown set owner

---
+++ 20091211/Assets/Scripts/Gun.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Gun : Base
+{
+    public enum Weapon { AntiGrawitaty }
+    Weapon weapon;
+    Quaternion q;
+    public int gravdist = 20;
+    public int grawforce = 20;
+    public bool MouseDown1;
+    void SetMouseDown(bool b)
+    {
+        if (MouseDown1 == b) return;
+        MouseDown1 = b;
+
+    }
+    Transform cur2 { get { return transform.Find("cursor2").transform; } }
+
+    protected override void Update()
+    {
+        if (IsMine) q = Find<Cam>().transform.rotation;
+        this.transform.rotation = q;
+        if (IsMine)
+            LocalUpdate();
+
+    }
+
+    private void LocalUpdate()
+    {
+        SetMouseDown(Input.GetMouseButton(1));
+        UpdateOwners();
+        if (MouseDown1 && weapon == Weapon.AntiGrawitaty)
+            foreach (GameObject a in GameObject.FindGameObjectsWithTag("Box"))
+            {
+                Box box = a.GetComponent<Box>();
+                if (box.owner != null && box.owner.isMine)
+                    box.rigidbody.AddExplosionForce(-grawforce, cur2.position, gravdist);
+            }
+    }
+    private void UpdateOwners()
+    {
+
+        foreach (GameObject a in GameObject.FindGameObjectsWithTag("Box"))
+            if (Input.GetMouseButtonDown(1) && Vector3.Distance(a.transform.position, cur2.position) < gravdist)
+                foreach (NetworkView b in a.GetComponents<NetworkView>())
+                    if (b.isMine)
+                    {
+                        Network.RemoveRPCs(Network.player, (int)Group.SetOwner);
+                        b.group = (int)Group.SetOwner;
+                        b.RPC("SetOwner", RPCMode.AllBuffered);
+                    }
+    }
+
+
+
+
+}
+
+
+
+
+
---
+++ 20091211/Assets/Scripts/Spawn.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum Group { PlView, Player, Default,Rig,SetOwner }    
+public class Spawn : Base
+{
+    public Transform _Player;
+
+    protected override void OnNetworkLoadedLevel()
+    {
+        NetworkInstantiate(_Player, Vector3.zero, Quaternion.identity, Group.Player);        
+    }
+    protected override void Start()
+    {
+
+        
+        
+    }
+
+    protected override void OnPlayerDisconnected(NetworkPlayer player)
+    {
+
+        foreach (GameObject a in GameObject.FindGameObjectsWithTag("Box"))
+            foreach (NetworkView v in a.GetComponents<NetworkView>())
+                if (v.owner == player) Destroy(v);
+        Network.DestroyPlayerObjects(player);
+        Network.RemoveRPCs(player);        
+    }
+    
+    private void Destroy(NetworkView nw)
+    {
+        nw.viewID = NetworkViewID.unassigned;
+        nw.enabled = false;
+        Component.Destroy(nw);
+
+    }
+
+    public void NetworkInstantiate(Object prefab, Vector3 position, Quaternion rotation, Group group)
+    {
+        Transform t = (Transform)Network.Instantiate(prefab, position, rotation, (int)group);
+        t.networkView.group = (int)group;
+        t.networkView.RPC("SetID", RPCMode.AllBuffered, Network.player);
+    }
+    
+}
+  
