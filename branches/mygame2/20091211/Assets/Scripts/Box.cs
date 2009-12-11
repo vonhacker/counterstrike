950155d579391beef73b309375b0adac0c447387
dorumonstr <dorumonstr@dd554dd6-d52b-11dd-a3c7-fda619b15ae3>
2009-12-11 18:35:08 +0000

    work 3 players , onmousedown set owner

---
+++ branches/mygame2/20091211/Assets/Scripts/Box.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Box : Base
+{
+    public NetworkView owner;
+    [RPC]
+    void SetOwner(NetworkMessageInfo a)
+    {
+        foreach (NetworkView b in this.GetComponents<NetworkView>())
+            b.observed = null;
+        a.networkView.observed = this.rigidbody;
+        owner = a.networkView;
+    } 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
