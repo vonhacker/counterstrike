9365178249056484b7fda33ef5da8ac87fcfdc1a
dorumonstr <dorumonstr@dd554dd6-d52b-11dd-a3c7-fda619b15ae3>
2009-12-14 06:55:28 +0000

    added mass rename,now assinging by Name

---
+++ mygame2/Assets/Editor/AddChild.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using UnityEditor;
+using System.Collections;
+
+public class AddChild : ScriptableObject
+{
+    [MenuItem("GameObject/Rename")]
+    static void MenuAddChild()
+    {
+        
+        Transform[] transforms = Selection.GetTransforms(SelectionMode.Unfiltered);
+        int i = 0;
+        foreach (Transform transform in transforms)
+        {
+            i++;
+            if (transform.GetComponent<Box>() != null)
+                transform.name = i.ToString();
+        }
+    }
+}
